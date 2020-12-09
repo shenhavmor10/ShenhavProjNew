3b5301d51f01a90c3eadbdef2cc5d76f513f2e65
shenhavmor10 <shenhavmor10@gmail.com>
2020-12-09 14:14:02 +0200

    Add gui to tools almost done

---
+++ shenhavArmyNew/ToolsExe/Test/test.c
@@ -0,0 +1,167 @@
+#pragma once
+#include "C:\Users\Shenhav\Desktop\shenhavArmyNew\tsetCCode\tsetCCode\Header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include<malloc.h>
+
+#define int32 int
+#define mychar char
+#define Eight 8
+#define Eight2 Eight
+#define mac(r) (r+2)
+#define newStructThing struct threadlocaleinfostruct
+static int num;
+int num;
+
+
+struct example1 {
+	int a;
+	int b;
+	int a;
+	struct example2 c;
+	int a;
+};
+struct example2 {
+	int a;
+};
+
+
+
+
+
+
+
+
+int*** a;
+	int*** s;
+int max = 5;
+/*
+* sends a message to a client
+}
+
+* @params:
+* podd - the initsdfsdfsdfsdf
+* odpdf - kkdlldkdkfkdf
+  whichis doing siht
+* a - size -the size of the buffer
+* retval - sdfsdf
+* @returns:
+* if the messege  djfdjfddn dfkdf mdfdk dfm
+*/
+static int* main(int* podd, int** odpdf, char a, char* retval)
+{
+	int num;
+	struct example1 temp;
+	temp.c.a = 2;
+	int num;
+	for (int i = 0; i < 6; i++)
+	{
+		struct example1 temp;
+	}
+	Person c;
+	struct example3 {
+		int a;
+		int b;
+	};
+	if (!podd && odpdf == null)
+	{
+		printf("sdfsdfsdf");
+	}
+	struct example3 d;
+	Person a;
+	int5* variable1 = 0;
+	int*** a;
+	int*** s;
+	int max = 5;
+	if (max)
+	{
+		int max2 = max;
+	}
+	//dfgdfgdfgdfg
+	//fgfgfgfgfg
+
+	char * pointer = malloc(sizeof(char) * 10);
+	//dfgdfgdfgdfg
+	//fgfgfgfgfg
+	if (pointer == NULL)
+	{
+		printf("bacdd");
+	}
+	/*
+	*ok
+	*/
+	char* runner = pointer;
+	
+	*retval = pointer;
+
+	/*                  sdfsdfsdfsd*/
+	char** pointer2 = calloc(10,sizeof(char));
+	/*                
+	sdfsdfsdfsd*/
+	if (!pointer2)
+	{
+		prinf("checked");
+	}
+	for (int i = 0; i < max; i++)
+	{
+		*runner = 'a';
+		runner++;
+	}
+	*runner = '\0';
+	char* pointer5 = alloc(sizeof(char) * 10);
+	char * pointer4=pointer5;
+	char* pointer6 = pointer4;
+	/*
+	sfdfs
+	sdfsfsdfdsd
+	dfsdsd
+	dfsdfsdf
+	*/
+	puts(pointer);
+	free(pointer6);
+	return 0;
+	spoi();
+	
+}
+/*
+* sends a message to a client.
+
+* @params:
+* none
+* @returns:
+* if the messege  djfdjfddn dfkdf mdfdk dfm
+*/
+void spoi()
+{
+	if (num == null)
+	{
+		printf("ssss");
+	}
+	*num = 0;
+	struct example3 e;
+}
+
+int popo(pdpd* sss)
+{
+	dfsdfsdf
+}
+
+void func_ex()
+{
+	char* name;
+	char* Fname;
+	if (name != null)
+	{
+	}
+	main(podd,odpdf, name, popopo);
+	Popo fname = name;
+	
+	fname = name;
+}
+//TODO:
+/*
+* Doc in h and c and static c
+* free other func
+* same pointer adress dose not need null
+* 
+*/
